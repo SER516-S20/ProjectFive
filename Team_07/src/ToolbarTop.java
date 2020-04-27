4cd18f7a6bf0a2e08889e9728257ce408fd219a4
adityab079 <36786132+adityab079@users.noreply.github.com>
2020-04-26 20:37:37 -0700

    Added JToolbar files

---
+++ Team_07/src/ToolbarTop.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Top JToolbar
+ * Contains the Operators
+ *
+ * @author Aditya Bajaj
+ * @since April 26, 2020
+ *
+ */public class ToolbarTop extends JToolBar{
+    final static List<String> OPERATORS = new ArrayList<>(Arrays.asList
+            ("(", ")", "<", ">", "@", "||", "-"));
+    final Dimension DIMENSION_PANEL_TOP = new Dimension(Database.SCREEN_SIZE.width, 100);
+
+
+    ToolbarTop() {
+        super();
+        setLayout(new FlowLayout(FlowLayout.CENTER, 70, 20));
+        setPreferredSize(DIMENSION_PANEL_TOP);
+        setBorder(BorderFactory.createMatteBorder(0, 0, 0, 2, Color.white));
+        setBackground(Database.THEME_BLUE);
+        addOperators();
+    }
+
+    /**
+     * Add operators to the jTop Toolbar
+     */
+    void addOperators() {
+        for (String opName : OPERATORS) {
+            Op op = (Op) add(FactoryOp.getOp(opName));
+         //   ListenersPanelLeft.addShapeListeners(op);
+            ListenersToolbarTop.addShapeListeners(op);
+
+        }
+    }
+
+}
