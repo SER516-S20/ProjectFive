94c40797398f5cf887ab86720734e42f270def84
Kartik Mathpal <kmathpal684@gmail.com>
2020-04-28 12:15:39 -0700

    adds files to controller package

---
+++ Team_08/src/Controller/ShapesEnum.java
@@ -0,0 +1,18 @@
+package Controller;
+
+/**
+ * This class consists of constants for the shapes.
+ *
+ * @author Anusha Singh
+ * @version 1.0
+ */
+
+public enum ShapesEnum {
+    OPENBRACKET,
+    CLOSEBRACKET,
+    LESSTHAN,
+    GREATERTHAN,
+    ATTHERATE,
+    TWOBARS,
+    HYPHEN
+}
