924e38ddb6d404c8785c5d3be82bec3bf90c6aca
Rohit <r4rohit002@gmail.com>
2020-04-30 21:01:37 -0700

    code cleanup

--- Team_02/src/Translate.java
+++ Team_02/src/Translate.java
@@ -9,99 +9,55 @@ import java.util.List;
  */
 public class Translate {
 	
-
 	private static RightPanelDataProcessor processor = new RightPanelDataProcessor	();
 	private static List<Icon> icons = new ArrayList<Icon>();
 	private static HashMap<String, List<Icon>> iconMap; 
-	
 	private static HashMap<Integer,String> iconStore = new HashMap<Integer,String>();
 	
-	public static void update(RightPanelDataProcessor _processor) {
-		processor = _processor;
-		
+	public static void processTranslate(RightPanelDataProcessor _processor) {
+		processor = _processor;	
 	}
 	
-	
 	public static void printTranslate() {
+		
 		List<Line> lines = processor.getLineList();
 		HashMap<String, List<Icon>> icons = processor.getIconMap();
 		iconMap = processor.getIconMap();
 		
-		storeAllPoints();
+		storeAllIcons();
 		
 		System.out.println(iconMap.size());
 		
 		for(int i=0;i<lines.size();i++) {
-			Dot s = lines.get(i).getStartDot();
-			Dot e = lines.get(i).getEndDot();
-			//System.out.println("Line: ("+s.getX()+","+s.getY()+") -->  ("+e.getX()+","+e.getY()+")");
-			compute(s.getX(), e.getX());
+			Dot start = lines.get(i).getStartDot();
+			Dot end = lines.get(i).getEndDot();
+			compute(start.getX(), end.getX());
 		}
-		
-		
-		
-		
-		
+			
 	}
 	
-	
-	
-	private static void storeAllPoints() {
-		
+	private static void storeAllIcons() {
 		
 		icons = new ArrayList<>(); 
-		//iconStore = new HashMap<Integer,String>();
-		
 		for(String key: iconMap.keySet()) {
-			
-			System.out.println("Key: "+key);
 			
 			List<Icon> iconsStored = iconMap.get(key);
 			
 			for(Icon icon: iconsStored) {
-				
-				System.out.println("YOYO"+icon.getTraslateSymbol());
-				
 				iconStore.put(icon.getCenterX(), icon.getTraslateSymbol());
 			}
 			
 			icons.addAll(iconsStored);
-			
-			
 		}
-		
-		
-		//displayAllPoints();
-		
-		
+				
 	}
 		
 	
-	private static void displayAllPoints() {
-		
-		System.out.println("------------------------------------------");
-		
-//		for(Icon icon:icons) {
-//			System.out.println(icon.getCenterX() +":"+ icon.getCenterY());
-//		r
-		
-		for(Integer key: iconStore.keySet()) {
-			System.out.println("Key: "+key);
-		}
-		
-		
-		System.out.println("------------------------------------------");
-	}
-	
-	
-	private static void compute(Integer sourceX, Integer targetX) {
-		
-	
-		
-		String s =iconStore.get(sourceX-187);
-		String t = iconStore.get(targetX-7);
-		System.out.println(s+ "is connected with"+ t);
-		
+	private static void compute(Integer source_x_cordinate, Integer target_x_cordinate) {
+
+		String source =iconStore.get(source_x_cordinate-187);
+		String target = iconStore.get(target_x_cordinate-7);
+		System.out.println(source+ " -> "+ target);	
 	}
 
 }
