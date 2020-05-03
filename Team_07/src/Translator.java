05db8ad03f5f4694f541b24a351a23b6557a0bd7
creativebuilder <csaravindt@gmail.com>
2020-05-02 18:59:50 -0700

    Code cleanup

--- Team_07/src/Translator.java
+++ Team_07/src/Translator.java
@@ -13,50 +13,35 @@ public class Translator {
     PanelRightTab currentTab = Database.selectedTab;
 
     public void translate() {
-
-        String code="digraph ";
         StringBuilder sb=new StringBuilder();
-        Set<Integer> hashset=new HashSet<Integer>();
-
+        Set<Integer> hashSet=new HashSet<Integer>();
         sb.append("digraph G {");
-
         List<Connector> src = Database.selectedTab.src;
         List<Connector> desc = Database.selectedTab.dest;
-        //Graph graph = new Graph(currentTab.getComponentCount());
-        //System.out.println("Inside translate function");
+
         for (int i = 0; i < src.size(); i++) {
             Op op1 = src.get(i).op;
             Op op2 = desc.get(i).op;
-            //int minID = MainFrame.PANEL_RIGHT.getSelectedIndex() == 0 ? 17 : 1;
-            //graph.addEdge(op1.ID-minID , op2.ID-minID);
-
-            //add your line here
-            hashset.add(op1.ID);
-            hashset.add(op2.ID);
+            hashSet.add(op1.ID);
+            hashSet.add(op2.ID);
             System.out.println("src "+op1.label+" id: "+op1.ID);
             System.out.println("dest "+op2.label+" id: "+op2.ID);
             sb.append("\"" + op1.label + " " + (op1.ID-17) +
-                    "\" -> \"s" + op2.label + " " + (op2.ID-17) + "\";" + "\n");
-
-
+                    "\" -> \"" + op2.label + " " + (op2.ID-17) + "\";" + "\n");
         }
 
         for (Component component :
                 currentTab.getComponents()) {
             Op op = (Op) component;
-            if(!hashset.contains(op.ID)){
+            if(!hashSet.contains(op.ID)){
                 sb.append("\"" + op.label + " " + (op.ID-17) + "\";" + "\n");
-                hashset.add(op.ID);
+                hashSet.add(op.ID);
             }
-            System.out.println("Components without connections");
-            System.out.println("src "+op.label+ " id: "+op.ID);
+
         }
 
-
         sb.append("}");
-
         System.out.println("Translated code is "+sb.toString());
-
     }
 
 }
