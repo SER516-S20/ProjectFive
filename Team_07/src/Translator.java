b2d70c68f7b8e5c31592df7da9ba92838927a398
creativebuilder <csaravindt@gmail.com>
2020-05-02 18:55:40 -0700

    Implemented translating the tab components into graphviz code

--- Team_07/src/Translator.java
+++ Team_07/src/Translator.java
@@ -1,5 +1,7 @@
 import java.awt.*;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Translator translates all the symbols and their connections into graphviz code
@@ -14,6 +16,8 @@ public class Translator {
 
         String code="digraph ";
         StringBuilder sb=new StringBuilder();
+        Set<Integer> hashset=new HashSet<Integer>();
+
         sb.append("digraph G {");
 
         List<Connector> src = Database.selectedTab.src;
@@ -27,13 +31,26 @@ public class Translator {
             //graph.addEdge(op1.ID-minID , op2.ID-minID);
 
             //add your line here
+            hashset.add(op1.ID);
+            hashset.add(op2.ID);
             System.out.println("src "+op1.label+" id: "+op1.ID);
             System.out.println("dest "+op2.label+" id: "+op2.ID);
-            sb.append("\""+op1.label+"\" -> \""+op2.label+"\";");
+            sb.append("\"" + op1.label + " " + (op1.ID-17) +
+                    "\" -> \"s" + op2.label + " " + (op2.ID-17) + "\";" + "\n");
 
 
         }
 
+        for (Component component :
+                currentTab.getComponents()) {
+            Op op = (Op) component;
+            if(!hashset.contains(op.ID)){
+                sb.append("\"" + op.label + " " + (op.ID-17) + "\";" + "\n");
+                hashset.add(op.ID);
+            }
+            System.out.println("Components without connections");
+            System.out.println("src "+op.label+ " id: "+op.ID);
+        }
 
 
         sb.append("}");
