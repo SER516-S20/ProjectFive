7e130f98b895c2aa0712c1a3e89a6188aabf082c
creativebuilder <csaravindt@gmail.com>
2020-05-02 17:12:36 -0700

    Created the translator class that translates the tab and its elements into equivalent GraphViz code

---
+++ Team_07/src/Translator.java
@@ -0,0 +1,40 @@
+import java.util.List;
+
+/**
+ * Translator translates all the symbols and their connections into graphviz code
+ *
+ * @author Aravind Thillai Villalan
+ * @since May 2nd, 2020
+ */
+public class Translator {
+    PanelRightTab currentTab = Database.selectedTab;
+
+    public void translate() {
+
+        String code="digraph ";
+        StringBuilder sb=new StringBuilder();
+        sb.append("digraph G {");
+
+        List<Connector> src = Database.selectedTab.src;
+        List<Connector> desc = Database.selectedTab.dest;
+        Graph graph = new Graph(currentTab.getComponentCount());
+        //System.out.println("Inside translate function");
+        for (int i = 0; i < src.size(); i++) {
+            Op op1 = src.get(i).op;
+            Op op2 = desc.get(i).op;
+            //int minID = MainFrame.PANEL_RIGHT.getSelectedIndex() == 0 ? 17 : 1;
+            //graph.addEdge(op1.ID-minID , op2.ID-minID);
+
+            //add your line here
+            System.out.println("src "+op1.label);
+            System.out.println("dest "+op2.label);
+
+
+        }
+        sb.append("}");
+
+        System.out.println("Translated code is "+sb.toString());
+
+    }
+
+}
